716a597ac61d2ec2ef3eef645bb3cf8d182e0147
bek <isakhmet.bek@cbc-group.kz>
2023-10-21 15:28:15 +0600

    установил необходимые зависимости и создал шаблонные компоненты на vue

---
+++ laravel/resources/js/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <Index></Index>
+</template>
+
+<script>
+
+import Index from './components/Index.vue'
+
+export default {
+    name: "App",
+    components: {
+        Index
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
