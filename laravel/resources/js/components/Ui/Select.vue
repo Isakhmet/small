dfc5ac99e06566591fcc7d1f72f890bf9f4ea2d5
bek <isakhmet.bek@cbc-group.kz>
2023-10-23 18:58:10 +0600

    фронт рекурсивный вывод категорий

---
+++ laravel/resources/js/components/Ui/Select.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="dropdown">
+        <p class="btn dropdown-toggle" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+            {{category}}
+        </p>
+        <ul class="dropdown-menu p-0" aria-labelledby="dropdownMenuLink">
+            <tree-view :treeData="treeData"/>
+        </ul>
+    </div>
+</template>
+
+<script>
+import TreeView from './TreeView.vue';
+import {mapState} from 'vuex';
+
+export default {
+    data() {
+        return {
+            treeData: [
+                {
+                    id: 1,
+                    label: 'Category 1',
+                    children: [
+                        {
+                            id: 3,
+                            label: 'Category 1.1',
+                            children: [
+                                {
+                                    id: 5,
+                                    label: 'Category 1.1.1',
+                                    children: [],
+                                }
+                            ],
+                        },
+                        {
+                            id: 4,
+                            label: 'Category 1.2',
+                            children: [],
+                        }
+                    ],
+                },
+                {
+                    id: 2,
+                    label: 'Category 2',
+                    children: [
+                        {
+                            id: 6,
+                            label: 'Category 2.1',
+                            children: [],
+                        }
+                    ]
+                }
+            ],
+            selectedOption: null,
+        };
+    },
+    components: {
+        TreeView,
+    },
+    computed: {
+        ...mapState('feedback', ["category"])
+    }
+};
+</script>
---
+++ laravel/resources/js/components/Ui/TreeView.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul>
+        <li v-for="node in treeData" :key="node.id">
+            <label class="dropdown-item" @click="changeCategory(node.label)">
+                {{ node.label }}
+            </label>
+            <tree-view :treeData="node.children" v-if="node.children.length > 0"/>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        treeData: Array
+    },
+    methods: {
+        changeCategory(label) {
+            this.$store.commit('feedback/setCategory', label)
+        }
+    }
+}
+</script>
+
+<style>
+
+ul, li {
+    cursor: pointer;
+    list-style: none;
+}
+
+label {
+    cursor: pointer;
+}
+
+</style>
